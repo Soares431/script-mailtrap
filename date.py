2ebc0def4aaa245ce392581818a03e1bccde0c4f
Lucas Soares <111359861+Soares431@users.noreply.github.com>
2026-09-15 14:26:52 +0000

    feacture: add logging e time, correção de erros e envio para o menino ney

---
+++ date.py
@@ -0,0 +1,2 @@
+USER = "5f43644bf4ac94"  #Preciso trocar essa parte por valores reais
+PASSWORD = "ea850e1ba2b4c6"
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import smtplib
 from email.message import EmailMessage
+import date
+import logging 
+import time
 
 SMTP_HOST = "sandbox.smtp.mailtrap.io"
 SMTP_PORT = 2525
-SMTP_USER = "COLOQUE_SEU_USUARIO_SMTP_AQUI"  #Preciso trocar essa parte por valores reais
-SMTP_PASSWORD = "COLOQUE_SUA_SENHA_SMTP_AQUI"   #Entre no Mailtrap → Email Sandbox → SMTP Settings / Show Credentials e procure as credenciais específicas do SMTP Sandbox.
+SMTP_USER = date.USER  
+SMTP_PASSWORD = date.PASSWORD 
 
 DESTINATARIO = "lucas.00000854565@unicap.br"
 
@@ -12,7 +15,7 @@ DESTINATARIO = "lucas.00000854565@unicap.br"
 def enviar_smtp():
     mensagem = EmailMessage()
 
-    mensagem["From"] = '"Albert Einstein" <einstein@exemplo.test>'
+    mensagem["From"] = '"Neyma" <meninoney@exemplo.test>'
     mensagem["To"] = DESTINATARIO
     mensagem["Subject"] = "Demonstração de SMTP"
 
@@ -26,10 +29,10 @@ SMTP real do Mailtrap.
 
 O campo From desta mensagem foi definido como:
 
-Albert Einstein <einstein@exemplo.test>
+Meu menino Ney <einstein@exemplo.test>
 
 Isso demonstra que o campo From é um cabeçalho da mensagem
-e, isoladamente, não comprova a identidade real do remetente.
+e, não comprova a identidade do remetente.
 
 Mecanismos como SPF, DKIM e DMARC são utilizados atualmente
 para ajudar na autenticação e validação de mensagens.
@@ -38,32 +41,35 @@ Esta é uma demonstração acadêmica de Redes de Computadores.
 """
     )
 
-    print("\n=== CONEXÃO SMTP ===")
+    print("\n=== SIMULAÇÃO DE ENVIO DE E-MAIL E CONEXÃO SMTP ===")
     print(f"Servidor: {SMTP_HOST}")
     print(f"Porta: {SMTP_PORT}")
-
+    time.sleep(3)
+    
     with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as servidor:
         servidor.set_debuglevel(1)
 
-        print("\n=== EHLO ===")
+        logging.log(logging.INFO, "\n=== EHLO ===")
         servidor.ehlo()
+        time.sleep(1)
 
         if servidor.has_extn("starttls"):
-            print("\n=== STARTTLS ===")
+            logging.log(logging.DEBUG, "\n=== STARTTLS ===")
             servidor.starttls()
             servidor.ehlo()
 
-        print("\n=== AUTENTICAÇÃO ===")
+        logging.log(logging.INFO, "\n=== AUTENTICAÇÃO ===")
         servidor.login(SMTP_USER, SMTP_PASSWORD)
 
-        print("\n=== MAIL FROM ===")
-        print("Envelope SMTP:", SMTP_USER)
+        logging.log(logging.INFO, "\n=== MAIL FROM ===")
+        logging.log(logging.INFO, "Envelope SMTP: {}".format(SMTP_USER))
 
-        print("\n=== RCPT TO ===")
-        print("Destinatário:", DESTINATARIO)
+        logging.log(logging.INFO, "\n=== RCPT TO ===")
+        logging.log(logging.INFO, "Destinatário: {}".format(DESTINATARIO))
 
-        print("\n=== DATA ===")
-        print("Transmitindo mensagem...")
+        logging.log(logging.INFO, "\n=== DATA ===")
+        logging.log(logging.INFO, "Transmitindo mensagem...")
+        time.sleep(5)
 
         servidor.send_message(
             mensagem,
@@ -71,18 +77,18 @@ Esta é uma demonstração acadêmica de Redes de Computadores.
             to_addrs=[DESTINATARIO]
         )
 
-        print("\nMensagem aceita pelo servidor SMTP.")
+        logging.log(logging.INFO, "\nMensagem aceita pelo servidor SMTP. - ENVIO CONCLUÍDO")
+        
 
 
 print("\n=== Demonstração SMTP ===")
 print("1 - Executar envio SMTP")
 print("0 - Sair")
+print("=========================")
+
 
 opcao = input("\nEscolha: ").strip()
 
-if opcao == "1":
-    enviar_smtp()
-elif opcao == "0":
-    print("Encerrando.")
-else:
-    print("Opção inválida.")
+if opcao == "1": enviar_smtp()
+elif opcao == "0": logging.log(logging.WARNING, "Encerrando.")
+else:  logging.error("Opção inválida.")
